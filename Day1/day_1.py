43143d002e50e5fd52c48bbc345b6343a74a503c
Thomas Fisher <tfis113@wgu.edu>
2022-12-02 14:05:37 -0700

    Adjust return value to return total calories

--- Day1/day_1.py
+++ Day1/day_1.py
@@ -13,14 +13,15 @@ def max_calorie_elf(list):
         if calorie_count(x) > max_calorie:
             max_calorie = calorie_count(x)
             max_elf = i + 1
-    return max_elf
+    elf_and_pos = [max_elf,max_calorie]
+    return elf_and_pos
 
 def main():
     input = open('Day1\input.txt','r')
     elven_calories = input.read()
     input.close()
     max_elf = max_calorie_elf(elven_calories)
-    print("Elf with most calories found: {}".format(max_elf))
+    print("Elf with most calories found at pos: {} with {} total calories in pack".format(max_elf[0],max_elf[1]))
 
 if __name__ == "__main__":
     main()   
