742246ba476f730157e07a2e494117f3f86e5668
Thomas Fisher <tfis113@wgu.edu>
2022-12-02 13:46:43 -0700

    Add example test case

--- Day1/day_1_tests.py
+++ Day1/day_1_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from day_1 import *
-
+example = '1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000'
 class test_max_calorie_elf(unittest.TestCase):
 
     def test_one_elf(self):
@@ -14,6 +14,9 @@ class test_max_calorie_elf(unittest.TestCase):
 
     def test_no_items(self):
         self.assertEqual(max_calorie_elf('0\n0\n\n0'), 0)
+
+    def test_example_given(self):
+        self.assertEqual(max_calorie_elf(example), 4)
 
 class test_calorie_count(unittest.TestCase):
 
